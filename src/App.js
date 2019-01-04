38c64f52fc5cc41014efa09d4fb0a71a63740e61
github username <crishanks12@gmail.com>
2019-01-03 21:21:34 -0700

    organized folders, props and state practice

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      txt: "this is the state text",
+      cat: 0
+    }
+  }
+
+  update(event) {
+    this.setState({txt: event.target.value})
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <input type="text"
+          onChange={this.update.bind(this)}/>
+        <h1>{this.state.txt} - {this.state.cat}</h1>
       </div>
-    );
+    )
   }
 }
-//hello
 
-export default App;
+export default App
